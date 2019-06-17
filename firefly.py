43c72254db4ea11d9d8b3e3c77044ec784102bd1
Federico Tondolo <ftondolo@uchicago.edu>
2019-06-17 16:54:06 -0400

    Progress
    Lightbulb status is now recognized from cropped image.  Next step is to change the result, rather than visualizing the state the program must now find out what frame this happens on and supply frame number for ffmpeg to trim (if that is indeed how it worls).

--- firefly.py
+++ firefly.py
@@ -1,18 +1,34 @@
 import numpy as np
 import cv2
 
-vid=cv2.VideoCapture('cap.wmv')
-ret, frame = vid.read()
-while(ret):
-    ret, frame = vid.read()
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(grayscale, (11, 11), 0)
-    # showing bright regions of blurred photo
-    brightest = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)[1]
-    # perform a series of erosions and dilations to remove
-    # any small blobs of noise from the thresholded image
-    brightest = cv2.erode(brightest, None, iterations=2)
-    brightest = cv2.dilate(brightest, None, iterations=4)
-    cv2.imshow('frame',brightest)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    #16.176 frames in the video
+    cap = cv2.VideoCapture("cap.wmv")
+    #Initialisation of frame_counting variable
+    frame_count=0;
+    #Loop as long as there's a frame to analyze
+    while cap.grab():
+        #Initialisation of supporting variables
+        flag, frame = cap.retrieve()
+        #Adding current frame to frame counter
+        frame_count+=1
+        print((frame_count))
+        imCrop = frame[80:95,150:175]
+        finder(imCrop)
+        #Escape
+        if cv2.waitKey(10) == 27:
+            break
+
+def finder(im):
+    # Set up the detector with necessary parameters
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByColor = 1
+    params.blobColor=255
+    detector = cv2.SimpleBlobDetector_create(params)
+    # Detect blobs
+    keypoints = detector.detect(im)
+    #Drawing red cicles around said blobs
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow("Keypoints", im_with_keypoints)
+
+main()
